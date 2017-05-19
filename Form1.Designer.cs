364dab0ae792a11b706bc8f90ddeecaffd8ef2ec
WIN_NT\x16j057 <x16j057@chiba-fjb.ac.jp>
2017-05-19 11:40:07 +0900

    if文

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.tbxYear = new System.Windows.Forms.TextBox();
+            this.btnJudge = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "西暦";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(141, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "年";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(96, 121);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 12);
+            this.lblResult.TabIndex = 2;
+            // 
+            // tbxYear
+            // 
+            this.tbxYear.Location = new System.Drawing.Point(71, 50);
+            this.tbxYear.Name = "tbxYear";
+            this.tbxYear.Size = new System.Drawing.Size(54, 19);
+            this.tbxYear.TabIndex = 3;
+            this.tbxYear.Text = "年";
+            this.tbxYear.TextChanged += new System.EventHandler(this.tbxYear_TextChanged);
+            // 
+            // btnJudge
+            // 
+            this.btnJudge.Location = new System.Drawing.Point(202, 101);
+            this.btnJudge.Name = "btnJudge";
+            this.btnJudge.Size = new System.Drawing.Size(33, 53);
+            this.btnJudge.TabIndex = 4;
+            this.btnJudge.Text = "判定";
+            this.btnJudge.UseVisualStyleBackColor = true;
+            this.btnJudge.Click += new System.EventHandler(this.btnJudge_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnJudge);
+            this.Controls.Add(this.tbxYear);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "うるう年";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox tbxYear;
+        private System.Windows.Forms.Button btnJudge;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,35 @@ namespace Example11
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbxYear_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnJudge_Click(object sender, EventArgs e)
+        {
+            int year = int.Parse(tbxYear.Text);
+
+            if (year % 4 != 0)
+                lblResult.Text = "①平成です";
+            else
+            {
+                if (year % 100 == 0)
+                {
+                    if (year % 400 == 0)
+                        lblResult.Text = "②うるう年です";
+                    else
+                        lblResult.Text = "③平成です";
+                }
+                else
+                    lblResult.Text = "④うるう年です";
+            }
+        }
     }
 }
